4c084b6cdebc2b9f6fef4e58539c036d06867325
danilojpfreitas <danilojpfreitas@gmail.com>
2023-05-29 15:29:41 -0300

    Terraform - Data Factory HTTP/DataLake Linked

--- Azure-Terraform/data_factory.tf
+++ Azure-Terraform/data_factory.tf
@@ -7,3 +7,112 @@ resource "azurerm_data_factory" "vacinacao" {
     environment = var.environment
   }
 }
+
+# Connection HTTP Server to Data Factory
+resource "azurerm_data_factory_linked_custom_service" "Data01" {
+  name                = "Data01"
+  data_factory_id = azurerm_data_factory.vacinacao.id
+  type            = "HttpServer"
+
+  type_properties_json = <<JSON
+{
+    "url": "https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SIPNI/COVID/uf/uf%3DAL/part-00000-a1ee0c9b-ec60-4d91-8c80-c4dc3014b27c.c000.csv",
+    "enableServerCertificateValidation": false,
+
+    "authenticationType": "Anonymous"
+}
+JSON
+
+  annotations = []
+
+}
+
+resource "azurerm_data_factory_linked_custom_service" "Data02" {
+  name                = "Data02"
+  data_factory_id = azurerm_data_factory.vacinacao.id
+  type            = "HttpServer"
+
+  type_properties_json = <<JSON
+{
+    "url": "https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SIPNI/COVID/uf/uf%3DAL/part-00001-a1ee0c9b-ec60-4d91-8c80-c4dc3014b27c.c000.csv",
+    "enableServerCertificateValidation": false,
+
+    "authenticationType": "Anonymous"
+}
+JSON
+
+  annotations = []
+
+}
+
+resource "azurerm_data_factory_linked_custom_service" "Data03" {
+  name                = "Data03"
+  data_factory_id = azurerm_data_factory.vacinacao.id
+  type            = "HttpServer"
+
+  type_properties_json = <<JSON
+{
+    "url": "https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SIPNI/COVID/uf/uf%3DAL/part-00002-a1ee0c9b-ec60-4d91-8c80-c4dc3014b27c.c000.csv",
+    "enableServerCertificateValidation": false,
+
+    "authenticationType": "Anonymous"
+}
+JSON
+
+  annotations = []
+
+}
+
+resource "azurerm_data_factory_linked_custom_service" "Data04" {
+  name                = "Data04"
+  data_factory_id = azurerm_data_factory.vacinacao.id
+  type            = "HttpServer"
+
+  type_properties_json = <<JSON
+{
+    "url": "https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SIPNI/COVID/uf/uf%3DAL/part-00003-a1ee0c9b-ec60-4d91-8c80-c4dc3014b27c.c000.csv",
+    "enableServerCertificateValidation": false,
+
+    "authenticationType": "Anonymous"
+}
+JSON
+
+  annotations = []
+
+}
+
+resource "azurerm_data_factory_linked_custom_service" "Data05" {
+  name                = "Data05"
+  data_factory_id = azurerm_data_factory.vacinacao.id
+  type            = "HttpServer"
+
+  type_properties_json = <<JSON
+{
+    "url": "https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SIPNI/COVID/uf/uf%3DAL/part-00004-a1ee0c9b-ec60-4d91-8c80-c4dc3014b27c.c000.csv",
+    "enableServerCertificateValidation": false,
+
+    "authenticationType": "Anonymous"
+}
+JSON
+
+  annotations = []
+
+}
+
+resource "azurerm_data_factory_dataset_http" "vacinacao" {
+  count = length(var.data_factory_linked_name)
+  name                = var.data_factory_linked_name[count.index]
+  data_factory_id     = azurerm_data_factory.vacinacao.id
+  linked_service_name = var.data_factory_linked_name[count.index]
+  depends_on = [ azurerm_data_factory_linked_custom_service.Data05 ]
+  
+  request_method = "GET"
+}
+
+# Connection Data Lake Gen 2 to Data Factory
+resource "azurerm_data_factory_linked_service_data_lake_storage_gen2" "vacinacao" {
+  name                  = "DataLakeLanding"
+  data_factory_id       = azurerm_data_factory.vacinacao.id
+  url                   = var.storage_account_url
+  storage_account_key   = var.storage_account_key
+}
--- Azure-Terraform/variables.tf
+++ Azure-Terraform/variables.tf
@@ -20,3 +20,20 @@ variable "containers_name" {
 variable "object_id" {
   type = string
 }
+
+#Data Factory
+variable "data_factory_linked_name" {
+  type = list(string)
+}
+
+variable "data_factory_linked_url" {
+  type = list(string)
+}
+
+variable "storage_account_key" {
+  type = string
+}
+
+variable "storage_account_url" {
+  type = string
+}
